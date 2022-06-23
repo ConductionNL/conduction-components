9627a13b8ea906c92090bdb2a43f7896f54de700
Hamid <gh.hamid89@gmail.com>
2022-06-23 13:25:32 +0200

    TopNav PR edit

--- src/components/topNav/TopNav.tsx
+++ src/components/topNav/TopNav.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import { Link } from "@gemeente-denhaag/components-react";
 import * as styles from "./TopNav.module.css";
-import { navigate } from "gatsby";
 import clsx from "clsx";
 
 export interface ITopNavItem {
   label: string;
-  href: string;
   icon?: JSX.Element;
-  linkState?: any;
+  handleClick (): any;
 }
 
 interface TopNavProps {
@@ -18,11 +16,11 @@ interface TopNavProps {
 
 export const PrimaryTopNav: React.FC<TopNavProps> = ({ items, layoutClassName }) => {
   return (
-    <div className={clsx([styles.primary, layoutClassName && layoutClassName])}>
+    <div className={clsx(styles.primary, layoutClassName && layoutClassName)}>
       <nav className={styles.primary}>
         <ul className={styles.ul}>
-          {items.map(({ label, href, icon, linkState }, idx) => (
-            <li className={styles.li} key={idx} onClick={() => navigate(href, { state: linkState })}>
+          {items.map(({ label,icon , handleClick }, idx) => (
+            <li className={styles.li} key={idx} onClick={() => handleClick()}>
               <Link className={styles.link} icon={icon} iconAlign="start">
                 {label}
               </Link>
@@ -36,16 +34,14 @@ export const PrimaryTopNav: React.FC<TopNavProps> = ({ items, layoutClassName })
 
 export const SecondaryTopNav: React.FC<TopNavProps> = ({ items, layoutClassName }) => {
   return (
-    <div className={clsx([styles.secondary, layoutClassName && layoutClassName])}>
+    <div className={clsx(styles.secondary, layoutClassName && layoutClassName)}>
       <nav>
         <ul className={styles.ul}>
-          {items.map(({ label, href, icon }, idx) => (
-            <li className={styles.li} key={idx}>
-              <a className={styles.anchor} href={href} target="_blank">
+          {items.map(({ label,  icon,handleClick }, idx) => (
+            <li className={styles.li} key={idx} onClick={()=>handleClick}>
                 <Link className={styles.link} icon={icon} iconAlign="start">
                   {label}
                 </Link>
-              </a>
             </li>
           ))}
         </ul>
