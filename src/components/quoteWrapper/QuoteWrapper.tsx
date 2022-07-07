651448170c025a41c1af69317f5d988eda55be25
Remko <remko@conduction.nl>
2022-07-07 11:49:53 +0200

    Refactored QuoteWrapper

--- src/components/quoteWrapper/QuoteWrapper.tsx
+++ src/components/quoteWrapper/QuoteWrapper.tsx
@@ -1,31 +1,15 @@
 import * as React from "react";
 import * as styles from "./QuoteWrapper.module.css";
-import clsx from "clsx";
 
 interface QuoteWrapperProps {
-  color?: string;
-  width?: string;
+  borderColor?: string;
+  borderWidth?: string;
 }
 
-export const QuoteWrapper: React.FC<QuoteWrapperProps> = ({ children, color, width }) => {
-  const borderStyle = document.getElementById("border");
-
-  if (borderStyle) {
-    if (color) {
-      borderStyle.style.borderLeft = `6px solid ${color}`;
-    }
-    if (width) {
-      borderStyle.style.borderLeft = `${width} solid #f5f5f5`;
-    }
-    if (width && color) {
-      borderStyle.style.borderLeft = `${width} solid ${color}`;
-    }
-  }
-
+export const QuoteWrapper: React.FC<QuoteWrapperProps> = ({ children, borderColor, borderWidth }) => {
   return (
-    <div className={styles.content}>
-      <div id="border" className={clsx(styles.virticalBorder, [width && borderStyle], [color && borderStyle])} />
-      <div className={styles.container}>{children}</div>
+    <div style={{ borderLeftColor: borderColor, borderLeftWidth: borderWidth }} className={styles.container}>
+      {children}
     </div>
   );
 };
