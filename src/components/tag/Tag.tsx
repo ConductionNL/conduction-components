f081fc9fd83627aebf15c415b0c61ea8f11b9f0d
Remko <remko@conduction.nl>
2022-08-16 12:08:59 +0200

    added optional icon

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -3,6 +3,12 @@ import * as styles from "./Tag.module.css";
 
 interface TagProps {
   tag: string;
+  icon?: JSX.Element;
 }
 
-export const Tag: React.FC<TagProps> = ({ tag }) => <span className={styles.tag}>{tag}</span>;
+export const Tag: React.FC<TagProps> = ({ tag, icon }) => (
+  <div className={styles.tag}>
+    {icon && <span>{icon}</span>}
+    <span>{tag}</span>
+  </div>
+);
