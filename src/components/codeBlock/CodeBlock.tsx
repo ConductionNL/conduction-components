4f2add086295b2e06b0dcf829b80fdde05d3f84c
Thijn <Ralkey@outlook.com>
2022-09-27 12:00:41 +0200

    added CodeBlock component

---
+++ src/components/codeBlock/CodeBlock.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as styles from "./CodeBlock.module.css";
+
+interface CodeBlockProps {
+  codeBlock: string | JSX.Element;
+}
+
+export const CodeBlock: React.FC<CodeBlockProps> = ({ codeBlock }) => <div className={styles.code}>{codeBlock}</div>;
