02d04732920bfaba36dc1a69790bb75bb48409c5
Lennart Bank <contact@lencodes.com>
2023-08-22 15:58:43 +0200

    REFACTOR CardWrapper component

--- src/components/card/cardWrapper/CardWrapper.tsx
+++ src/components/card/cardWrapper/CardWrapper.tsx
@@ -1,13 +1,9 @@
 import * as React from "react";
 import * as styles from "./CardWrapper.module.css";
 import { HTMLAttributes } from "react";
-import clsx from "clsx";
 
 export const CardWrapper = (props: React.HTMLAttributes<HTMLDivElement>): JSX.Element => {
+  const _props = { ...props, className: `${props.className} ${styles.container}` };
 
-  return (
-    <div className={clsx(styles.container, props.className && props.className)} {...props}>
-      {props.children}
-    </div>
-  );
+  return <div {..._props}>{props.children}</div>;
 };
