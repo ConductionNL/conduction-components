f3978b146776418fee2d440afa5039090bf20396
Remko <remko@conduction.nl>
2023-08-23 18:11:00 +0200

    Updated CardWrapper

--- src/components/card/cardWrapper/CardWrapper.tsx
+++ src/components/card/cardWrapper/CardWrapper.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as styles from "./CardWrapper.module.css";
-import { HTMLAttributes } from "react";
 
 export const CardWrapper = (props: React.HTMLAttributes<HTMLDivElement>): JSX.Element => {
   const _props = { ...props, className: `${props.className} ${styles.container}` };
