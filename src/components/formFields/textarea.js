d31ce7c1321c8c0ea77ec49f40279cc0e7f5df1f
Lennart Bank <contact@lencodes.com>
2023-09-05 09:18:11 +0200

    REMOVE generated .js files

--- src/components/formFields/textarea.js
+++
@@ -1,3 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { TextArea } from "@gemeente-denhaag/textarea";
-export const Textarea = ({ name, validation, register, errors }) => (_jsx(TextArea, { ...register(name, { ...validation }), invalid: errors[name] }));
