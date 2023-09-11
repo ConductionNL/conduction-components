9012079ac6ae127484570d694d96303cffbfc310
Remko <remko@conduction.nl>
2023-09-11 10:50:22 +0200

    WIP changes to tabs

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as styles from "./Tabs.module.css";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { BadgeCounter } from "@utrecht/component-library-react";
+import clsx from "clsx";
 
 interface testProps {
   tabs: ITabProps[];
@@ -21,7 +22,7 @@ export const TabContext: React.FC<testProps> = ({ tabs, tabContent }) => {
     <Tabs className={styles.tabs}>
       <TabList className={styles.tabList}>
         {tabs.map(({ name, badge }) => (
-          <Tab className={styles.tab}>
+          <Tab className={clsx(styles.tab, badge && styles.tabBadge)}>
             <span>
               {name}
               {badge !== undefined && <BadgeCounter className={styles.badge}>{badge}</BadgeCounter>}
@@ -30,7 +31,7 @@ export const TabContext: React.FC<testProps> = ({ tabs, tabContent }) => {
         ))}
       </TabList>
       {tabContent.map(({ content }) => (
-        <TabPanel className={styles.tabPanel}>{content}</TabPanel>
+        <TabPanel>{content}</TabPanel>
       ))}
     </Tabs>
   );
