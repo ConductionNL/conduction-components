38f0f9527b497c1654e3dfea510a7e1039ee468c
Remko <remko@conduction.nl>
2023-11-22 09:22:43 +0100

    cleanup

--- src/components/formFields/textarea.tsx
+++ src/components/formFields/textarea.tsx
@@ -2,12 +2,10 @@ import _ from "lodash";
 import { IReactHookFormProps } from "./types";
 import { ErrorMessage } from "./errorMessage/ErrorMessage";
 import { Textarea as UtrechtTextarea } from "@utrecht/component-library-react/dist/css-module";
-import { Control, FieldValues } from "react-hook-form";
 
 export interface ITextAreaProps {
   name: string;
   ariaLabel: string;
-  control: Control<FieldValues, any>;
   disabled?: boolean;
   defaultValue?: string;
   hideErrorMessage?: boolean;
@@ -22,7 +20,6 @@ export const Textarea = ({
   defaultValue,
   hideErrorMessage,
   ariaLabel,
-  control,
 }: ITextAreaProps & IReactHookFormProps): JSX.Element => (
   <>
     <UtrechtTextarea
